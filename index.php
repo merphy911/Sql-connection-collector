637086b63031692139ed46dcd126ced30d592b10
merphy911 <64337218+merphy911@users.noreply.github.com>
2021-09-17 12:53:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "test";
+$connect = mysqli_connect($host, $user, $password, $db);
+
+function sql_query($DBname, $request, $DBtable){
+	global $connect;
+	$query = "SELECT * FROM "."`$DBname`";
+	$x = false;
+	$i = 0;
+	foreach($request as $base){
+		if($base){
+			if($x){
+				$query = $query." AND ".$DBtable[$i]." = ".$base;
+			}else{
+				$query = $query." WHERE ".$DBtable[$i]." = ".$base;
+				$x = true;
+			}
+		}
+		$i += 1;		
+	}
+	$base = mysqli_query($connect, $query);
+	$base = mysqli_fetch_all($base);
+	return $base;
+}
+
+
+//request (GET, POST)
+$request= ['test@mail.ru', 'Anna', 'Ermolova'];
+//DB struct
+$DBtable = ['email', 'name', 'surname'];
+
+$z = sql_query('boxs', $y, $x);
+print_r($z)
+?>
